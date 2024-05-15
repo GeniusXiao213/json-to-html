fe06d73b2bdf00f32527546390e76947ef175bb8
Tanya <tmehra99@gmail.com>
2024-05-15 12:11:20 +0200

    Search box

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React from 'react';
 import './App.css';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/header';
 import MainArea from './components/main-area/MainArea';
 import Box from '@mui/material/Box';
+import Login from './pages/Login';
 
 function App() {
   return (
     <Router>
       <Header />
+      <Routes>
+          <Route exact path="/login" element={<Login />} />
+      </Routes>
       <Box className="App">
       <MainArea />
-    </Box>
+      </Box>
     </Router>
   );
 }
