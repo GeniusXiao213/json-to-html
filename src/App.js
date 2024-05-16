05f1283c5cf55314f40273bab6af4cd18541cbac
Tanya <tmehra99@gmail.com>
2024-05-16 16:28:45 +0200

    Initial commit Merge of master+searchBox

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import Header from './components/header';
 import MainArea from './components/main-area/MainArea';
 import Box from '@mui/material/Box';
 import Login from './pages/Login';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
-    <Router>
-      <Header />
-      <Routes>
-          <Route exact path="/login" element={<Login />} />
-      </Routes>
-      <Box className="App">
-      <MainArea />
-      </Box>
-    </Router>
+    <Box className="App">
+      <Router>
+        <Header />
+        <Routes>
+            <Route exact path="/login" element={<Login />} />
+        </Routes>
+        <MainArea />
+      </Router>
+    </Box>    
   );
 }
 
--- src/components/main-area/text-area/TextArea.jsx
+++ src/components/main-area/text-area/TextArea.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Introduction from './text-area-components/introduction/Introduction'
 import Box from '@mui/material/Box';
 import Coverage from './text-area-components/coverage/Coverage';
+import Partner from './text-area-components/Partner';
 
 function TextArea() {
   return (
-    <Box>
+    <Box className='text-area'>
         <Introduction />
         <Coverage />
+        <Partner />
     </Box>
   )
 }
