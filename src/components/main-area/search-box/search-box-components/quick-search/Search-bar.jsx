432a8c39c9d46f756d5cec2fa1b0807bf9d78549
GeniusXiao213 <100718931+GeniusXiao213@users.noreply.github.com>
2024-05-18 15:52:57 +0200

    mistake

--- src/components/main-area/search-box/search-box-components/quick-search/Search-bar.jsx
+++ src/components/main-area/search-box/search-box-components/quick-search/Search-bar.jsx
@@ -40,31 +40,29 @@ export const SearchBar = () => {
 }
 
   return (
-    <Box>
-      <div>
-          <div className="input-wrapper" >
-            <SearchIcon className="search-icon" onClick={handleSubmit}/>
-            {/* Company or Person */}
-            <input placeholder="Company or Person" value={input} onChange={(e) => setInput(e.target.value)}/>
-          </div>
-          <div className="quick-search-filter">
-            <div>
-            <InfoOutlinedIcon className="info-icon" />
-            <input name="countries" type="hidden" class="noselection"/>
-            <Select
-            variant="standard"
-            disableUnderline="true"
-            className="country-filter"
-            labelId="country-filter-label"
-            id="country-filter"
-            label="all countries"
-            name="country-filter"
-            >
-              <MenuItem>United Kingdom</MenuItem>
-              <MenuItem>Austria</MenuItem>
-              <MenuItem>Belgium</MenuItem>
-            </Select>
-            </div>
+    <div>
+        <div className="input-wrapper" >
+          <SearchIcon className="search-icon" />
+          {/* Company or Person */}
+          <input placeholder="公司或个人" />
+        </div>
+        <div className="quick-search-filter">
+          <div>
+          <InfoOutlinedIcon className="info-icon" />
+          <input name="countries" type="hidden" class="noselection"/>
+          <Select
+          variant="standard"
+          disableUnderline="true"
+          className="country-filter"
+          labelId="country-filter-label"
+          id="country-filter"
+          label="all countries"
+          name="country-filter"
+          >
+            <MenuItem>United Kingdom</MenuItem>
+            <MenuItem>Austria</MenuItem>
+            <MenuItem>Belgium</MenuItem>
+          </Select>
           </div>
       </div>
     </Box>
